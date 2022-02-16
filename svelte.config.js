18c06d3933fcc7c38f2e8bf257bdf0dea6566cb3
jasonandrew <jasonxandrewth@gmail.com>
2022-02-16 14:39:35 +0100

    back to auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
-// import adapter from '@sveltejs/adapter-auto';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import sveltePreprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: vercel(),
+		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
